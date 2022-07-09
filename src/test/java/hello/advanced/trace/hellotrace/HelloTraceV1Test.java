bc2b99c5268707ccbcc7dd9eeeea34c4990f7e14
lllilllilllilili <hik3562@hufs.ac.kr>
2022-07-09 18:28:59 +0900

    220709 | 로그 추적기 기본 코드 작성

---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,21 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+class HelloTraceV1Test {
+
+    @Test
+    public void dummy() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void beign_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+}
